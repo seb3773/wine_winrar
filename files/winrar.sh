e341640ed8a326d3425a40a8569715a69084092d
seb3773 <99963207+seb3773@users.noreply.github.com>
2024-05-30 18:07:45 +0200

    Add files via upload

--- files/winrar.sh
+++ files/winrar.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if [ -n "$1" ]; then
 WINEPREFIX="$HOME/.win64" wine "$HOME/.win64/drive_c/Program Files/WinRAR/WinRAR.exe" "$(winepath -w "$1")" </dev/null >/dev/null 2>&1
+else
+WINEPREFIX="$HOME/.win64" wine "$HOME/.win64/drive_c/Program Files/WinRAR/WinRAR.exe" </dev/null >/dev/null 2>&1
+fi
